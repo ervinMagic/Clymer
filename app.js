31bf78d81f15e1e5b05be516094ea220d73699c4
Ervin Puskar <ervin.puskar@watershedlrs.com>
2019-08-18 13:08:13 -0500

    Add bootstrap starter

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ app.set('view engine', 'handlebars');
 app.get('/', function (req, res) {
     res.render('home', {
         showTitle: true,
+        title: "Clymer Test App",
         foo: "Test App",
         bar: function () {
             return "Another test"
@@ -26,68 +27,5 @@ app.get('/', function (req, res) {
 
 app.listen(port);
 
-// var server = http.createServer(function (req, res) {
-//     if (req.method === 'POST') {
-//         var body = '';
-//
-//         req.on('data', function(chunk) {
-//             body += chunk;
-//         });
-//
-//         req.on('end', function() {
-//             if (req.url === '/') {
-//                 log('Received message: ' + body);
-//             } else if (req.url = '/scheduled') {
-//                 log('Received task ' + req.headers['x-aws-sqsd-taskname'] + ' scheduled at ' + req.headers['x-aws-sqsd-scheduled-at']);
-//             }
-//
-//             res.writeHead(200, 'OK', {'Content-Type': 'text/plain'});
-//             res.end();
-//         });
-//     } else {
-//         res.writeHead(200);
-//         res.write(html);
-//         res.end();
-//     }
-// });
-
-// Listen on port 3000, IP defaults to 127.0.0.1
-// server.listen(port);
-
 // Put a friendly message on the terminal
 console.log('Server running at http://127.0.0.1:' + port + '/');
-
-
-
-// var path = require('path');
-// var cookieParser = require('cookie-parser');
-// var logger = require('morgan');
-//
-// var indexRouter = require('./routes/index');
-// var usersRouter = require('./routes/users');
-//
-// var app = express();
-//
-// app.engine('handlebars', exphbs());
-// app.set('view engine', 'handlebars');
-//
-// app.get('/', function (req, res) {
-//     res.render('home', {
-//         showTitle: true,
-//         foo: "Test App",
-//         bar: function () {
-//             return "Another test"
-//         }
-//     });
-// });
-//
-// app.use(logger('dev'));
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
-//
-// app.use('/', indexRouter);
-// app.use('/users', usersRouter);
-//
-// module.exports = app;
